df4a6339d3329491ad7e4be0dfd9ef2af0d68296
elif sinem aydin <elif.aydin@siemens.com>
2022-06-08 18:30:45 +0300

    git

--- src/main/java/Assigment/book/BookApplication.java
+++ src/main/java/Assigment/book/BookApplication.java
@@ -3,6 +3,15 @@ package Assigment.book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*
+git init
+git add -A
+git commit -m 'Added my project'
+git remote add origin git@github.com:sammy/my-new-project.git
+git push -u -f origin master
+ */
+
 @SpringBootApplication
 public class BookApplication {
 
